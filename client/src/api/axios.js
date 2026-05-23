ccf7e150297aee13c11d6d8d4e05497737ff3bda
GlindaRose <45208607+GlindaRose@users.noreply.github.com>
2026-05-23 12:47:02 +0100

    Link fix for CRA setup

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  bbaseURL: import.meta.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // ========================
